c08a9e7907a7a1647a5d93147374fc3bd3f0513f
Ethan Hanlon <ehanlon19@gmail.com>
2025-10-04 11:54:55 -0700

    Got a kinda-sorta-not-really map of half the Moon going

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import TileLayer from 'ol/layer/Tile'
+import { OSM, WMTS } from 'ol/source'
+import { get as getProjection } from 'ol/proj'
+import { getTopLeft, getWidth } from 'ol/extent'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import WMTSTileGrid from 'ol/tilegrid/WMTS'
 
-function App() {
-  const [count, setCount] = useState(0)
+export function Home() {
+  const projection = getProjection('EPSG:3857');
+  const projectionExtent = projection!.getExtent(); // FIXME: Object is possibly 'null'.
+  const size = getWidth(projectionExtent) / 256;
+  const resolutions = new Array(19);
+  const matrixIds = new Array(19);
+  for (let z = 0; z < 19; ++z) {
+    // generate resolutions and matrixIds arrays for this WMTS
+    resolutions[z] = size / Math.pow(2, z);
+    matrixIds[z] = z;
+  }
+  // const [count, setCount] = useState(0)
+  new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new WMTS({
+          url: 'https://trek.nasa.gov/tiles/Moon/EQ/LRO_WAC_Mosaic_Global_303ppd_v02/1.0.0/default/default028mm/0/0/0.jpg',
+          layer: 'LRO_WAC_Mosaic_Global_303ppd_v02',
+          matrixSet: 'default028mm',
+          format: 'image/jpeg',
+          tileGrid: new WMTSTileGrid({
+            origin: getTopLeft(projectionExtent),
+            resolutions: resolutions,
+            matrixIds: matrixIds,
+          }),
+          style: 'default',
+          wrapX: true,
+        })
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div id="map" className="map" style={{ width: '100%', height: '400px' }}></div> */}
     </>
   )
 }
-
-export default App
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Home } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
