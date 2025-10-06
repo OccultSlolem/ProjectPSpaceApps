4bfc27dc473185b6672cdbbf559f5095a0cfd7b0
Ethan Hanlon <ehanlon19@gmail.com>
2025-10-05 17:52:03 -0700

    Add NavigationPanel

---
+++ Frontend/src/components/NavigationPanel.tsx
@@ -0,0 +1,93 @@
+import { useRef, useState } from "react";
+import { queryPlanetaryNavigationAssistant } from "../AIHelper";
+import type { Map } from "ol";
+
+export default function NavigationPanel({ map }: { map: Map | null }) {
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [navigationTextResponse, setNavigationTextResponse] = useState<string | null>(null);
+
+  async function submitNavigationQuery() {
+    if (!dialogRef.current || !inputRef.current) {
+      console.error('I seem to have encountered an impossible state. Please try again later.');
+      return;
+    }
+    console.log(map);
+    dialogRef.current?.close();
+    if (!map) {
+      alert('Map not loaded yet. Please try again later.');
+      return;
+    }
+
+    setLoading(true);
+    setNavigationTextResponse(null);
+    const response = await queryPlanetaryNavigationAssistant({ planet: 'moon', feature: inputRef.current?.value || '' });
+    setLoading(false);
+    switch (response.localError) {
+      case 'inputTooLong':
+        alert('Input too long. Please limit to 1000 characters.');
+        break;
+      case 'inputTooShort':
+        alert('Input cannot be empty. Please enter a feature name.');
+        break;
+      default:
+        break;
+    }
+
+    if (response.networkError) {
+      alert('Network error. Please try again later.');
+      return;
+    }
+
+    if (
+      !response.response?.foundFeature || 
+      !response.response?.latitude || 
+      !response.response?.longitude
+    ) {
+      alert('Could not find feature. Please try a different query.');
+      setNavigationTextResponse(response.response?.textResponse || 'Could not find feature.');
+      return;
+    }
+
+    map.getView().animate({
+      center: [response.response.longitude, response.response.latitude],
+      zoom: 6,
+      duration: 2000,
+    });
+    setNavigationTextResponse(response.response.textResponse);
+
+    dialogRef.current?.close();
+  }
+
+  return (
+    <>
+      <div className="panel">
+        <h1>Project P - Lunar Map</h1>
+        <p>This is a map of the Moon using NASA's LRO WAC Mosaic.</p>
+        <button onClick={() => dialogRef.current?.showModal()}>Navigation Assistant 🪐</button>
+        {/* <button onClick={testFetchA}>Test Fetch (Feature)</button>
+        <button onClick={testFetchB}>Test Fetch (Navigation)</button> */}
+        {loading && <p>Loading...</p>}
+        {navigationTextResponse && 
+          <>
+            <hr />
+            <p>{navigationTextResponse}</p>
+            <button onClick={() => setNavigationTextResponse(null)}>Clear</button>
+          </>}
+      </div>
+
+      <dialog ref={dialogRef} id="panelDialog">
+        <h2 id="featureName">Planetary Navigation Assisant 🪐</h2>
+        <p id="featureDescription">Tell me where you want to go, and I'll take you there!</p>
+        <textarea ref={inputRef} id="navFeatureTextArea" placeholder="Enter feature name" />
+        <button style={{ marginLeft: '10px' }} onClick={submitNavigationQuery}>Submit</button>
+        <hr />
+        <button onClick={() => {
+          const dialog = dialogRef.current;
+          dialog?.close();
+        }}>Close</button>
+      </dialog>
+    </>
+  )
+}
